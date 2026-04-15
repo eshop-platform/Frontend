6c1b22a4f590c04b0178d1a90f6d08439f51866e
MaryaTawfik <maryaahmed200412@gmail.com>
2026-04-15 10:59:21 +0200

    feat: initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
+import path from "path" // Import this at the top
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
